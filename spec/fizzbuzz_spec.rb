b091ff82426f07ae124d8beb6907341f28c6e8cc
Ed Perkins <ed@edperkins.co.uk>
2017-12-21 11:43:10 +0000

    add test for multiples of 3 & 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,10 +7,24 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)). to eq 'buzz'
   end
-  it 'returns "fizz" when passed multiple of 3' do
+  it 'returns "fizz" when passed multiple of 3 that is not also a multiple of 5' do
     (1..20).each {|number|
-      if number % 3 == 0
+      if (number % 3 == 0) && (number % 5 != 0)
       expect(fizzbuzz(number)).to eq 'fizz'
+      end
+    }
+  end
+  it 'returns "buzz" when passed multiple of 5 that is not also a multiple of 3' do
+    (1..20).each {|number|
+      if (number % 5 == 0) && (number % 3 != 0)
+      expect(fizzbuzz(number)).to eq 'buzz'
+      end
+    }
+  end
+  it 'returns "fizzbuzz" when passed multiple of 3 & 5' do
+    (1..20).each {|number|
+      if (number % 3 == 0) && (number % 5 == 0)
+      expect(fizzbuzz(number)).to eq 'fizzbuzz'
       end
     }
   end
