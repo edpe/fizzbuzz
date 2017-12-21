02e6bccd991b370a37d30489f02bca945081547d
Ed Perkins <ed@edperkins.co.uk>
2017-12-21 14:04:28 +0000

    refactored to one line

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
+#def fizzbuzz(number)
+
+#  return "fizzbuzz" if (number % 3 == 0) && (number % 5 == 0)
+
+#  return "fizz" if number % 3 == 0
+
+#  return "buzz" if number % 5 == 0
+
+#  number
+
+#end
+
 def fizzbuzz(number)
-  
-  return "fizzbuzz" if (number % 3 == 0) && (number % 5 == 0)
-
-  return "fizz" if number % 3 == 0
-
-  return "buzz" if number % 5 == 0
-
-  number
-
+  number % 3 == 0 ? (number % 5 == 0 ? "fizzbuzz" : "fizz") : (number % 5 == 0 ? "buzz" : number)
 end
