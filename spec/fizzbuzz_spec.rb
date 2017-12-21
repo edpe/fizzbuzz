711918cb0c38623fd3885ba830410139c196636d
Ed Perkins <ed@edperkins.co.uk>
2017-12-21 11:29:05 +0000

    works with multiples of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,14 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)). to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)). to eq 'buzz'
+  end
+  it 'returns "fizz" when passed multiple of 3' do
+    (1..20).each {|number|
+      if number % 3 == 0
+      expect(fizzbuzz(number)).to eq 'fizz'
+      end
+    }
+  end
 end
